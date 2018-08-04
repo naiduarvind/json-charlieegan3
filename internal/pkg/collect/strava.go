ab5d49ec7fa6c2198df82904cd3f604d2851844c
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-08-04 18:53:13 +0100

    Add strava collector

---
+++ internal/pkg/collect/strava.go
@@ -0,0 +1,64 @@
+package collect
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+type activity struct {
+	AverageHeartrate float64 `json:"average_heartrate"`
+	Distance         float64 `json:"distance"`
+	MovingTime       int64   `json:"moving_time"`
+	Name             string  `json:"name"`
+	StartDate        string  `json:"start_date"`
+	Type             string  `json:"type"`
+}
+
+// LatestActivity wraps deta about the latest activity
+type LatestActivity struct {
+	AverageHeartrate float64   `json:"average_heartrate"`
+	Distance         float64   `json:"distance"`
+	MovingTime       int64     `json:"moving_time"`
+	Name             string    `json:"name"`
+	CreatedAt        time.Time `json:"created_at"`
+	Type             string    `json:"type"`
+}
+
+// Strava returns details about the most recent strava activity
+func Strava(host string) (LatestActivity, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/api/v3/athlete/activities", host))
+	if err != nil {
+		return LatestActivity{}, errors.Wrap(err, "get activities failed")
+	}
+
+	defer resp.Body.Close()
+
+	var activities []activity
+	err = json.NewDecoder(resp.Body).Decode(&activities)
+	if err != nil {
+		return LatestActivity{}, errors.Wrap(err, "body unmarshal failed")
+	}
+
+	if len(activities) == 0 {
+		return LatestActivity{}, errors.New("no activities found")
+	}
+
+	activity := activities[0]
+	createdAt, err := time.Parse(time.RFC3339, activity.StartDate)
+	if err != nil {
+		return LatestActivity{}, errors.Wrap(err, "latest activity time parsing failed")
+	}
+
+	return LatestActivity{
+		AverageHeartrate: activity.AverageHeartrate,
+		Distance:         activity.Distance,
+		MovingTime:       activity.MovingTime,
+		Name:             activity.Name,
+		Type:             activity.Type,
+		CreatedAt:        createdAt,
+	}, nil
+}
---
+++ internal/pkg/collect/strava_test.go
@@ -0,0 +1,41 @@
+package collect
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestStrava(t *testing.T) {
+	localServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		content, err := ioutil.ReadFile("strava_response.json")
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Fprint(w, string(content))
+	}))
+
+	result, err := Strava(localServer.URL)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if result.Name != "Evaporate" {
+		t.Error(result)
+	}
+	if result.Distance != 4231.5 {
+		t.Error(result)
+	}
+	if result.MovingTime != 1470 {
+		t.Error(result)
+	}
+	if result.AverageHeartrate != 142.9 {
+		t.Error(result)
+	}
+	if result.Type != "Run" {
+		t.Error(result)
+	}
+}
